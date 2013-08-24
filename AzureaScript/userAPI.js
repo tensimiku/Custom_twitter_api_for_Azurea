2e71e480e97ca95875d834da49134bccf0bfaf23
tensimiku <tensimiku@gmail.com>
2013-08-24 23:30:58 +0900

    fix bug caused by upgrading from azurea 1.4.7

--- AzureaScript/userAPI.js
+++ AzureaScript/userAPI.js
@@ -1,11 +1,10 @@
-﻿//
+//
 // Azurea with Mobile Web Js with App
 // By. Itsurea
 //
 
-//원 저작자가 제가아닙니다.
-//고로 배포하시면 원저작자가 싫어할 가능성이 있습니다.
-//하지만 너무 잘만든 스크립트라서 수정해서 사용했습니다!
+//제가 짠 프로그램을 배포하는건 상관없지만
+//이 스크립트는 제가 원저작자가 아니니 주의해주세요!
 //뭔가 안된다! 하면 err.txt파일이 생길텐데 그걸 @tensimiku로 보내주세요!
 //配布するのはかまわないが元の製作者がいやがる可能性があります。
 //上の件は適宜処理してください。
@@ -14,7 +13,6 @@
 
 
 var otherchk = System.settings.getValue('user.API', 'Status');
-var mobilechk = System.settings.getValue('user.Mobile', 'Status');
 
 if (otherchk == 0 | !otherchk) {
 	System.showNotice('현재 Azurea 트윗을 사용하고 있습니다.');
@@ -25,16 +23,17 @@ function PreSendUpdateStatus(status) {
 	if (status.text.match('Hash(\\d+)=(.*)')) {
 		return;
 	}
+
 	if (status.text.match('Fillter=(.*)')) {
 		return;
 	}
+
 	if (otherchk == 0 | !otherchk) {
 		return;
 	}
-	if(mobilechk == 1 | !mobilechk) {
-		return;
-	}
+
 	var cont = '"'+ encodeURIComponent(status.text) +'"';
+	cont = cont.replace('*', '%2A');
 	var args = (status.in_reply_to_status_id ? status.in_reply_to_status_id : '') + ' ' + cont;
 	TextArea.text = '';
 	TextArea.hide();
